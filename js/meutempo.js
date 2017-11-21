f4264ad832a6506eec0888b24991f29b3362dc85
embuscadocodigo <31712288+embuscadocodigo@users.noreply.github.com>
2017-11-21 18:34:13 -0200

    up JS meutempo.js

--- js/meutempo.js
+++ js/meutempo.js
@@ -1,1 +1,21 @@
-criando arquivo
+var tempo = 900;
+function aguarda() {
+if (tempo >= 0) {
+	var el = document.getElementById("inicio");
+		if( el ){
+			secondscamp = parseInt(tempo / 1);
+			hours2 = parseInt(secondscamp / 3600);
+			secondscamp = secondscamp % 3600;
+			minutes2 = parseInt(secondscamp / 60);
+			secondscamp = secondscamp % 60;
+			el.innerHTML = minutes2 + ":" + secondscamp;
+		}
+		tempo = tempo - 1;
+		setTimeout("aguarda()", 1000);
+		}if (tempo == 0) {
+            var v = document.getElementsByTagName("audio")[0];
+            v.play();
+            }else{
+            } 
+}
+aguarda();
