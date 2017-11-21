ea117d58cf234b5f3aa2af891d7dd899fe1b2498
embuscadocodigo <31712288+embuscadocodigo@users.noreply.github.com>
2017-11-21 15:01:07 -0200

    cronometro musica duvida

---
+++ meucronometro.js
@@ -0,0 +1,23 @@
+var tempo = 10;
+function aguarda() {
+if (tempo >= 0) {
+	var el = document.getElementById("inicio");
+		if( el ){
+			secondscamp = parseInt(tempo / 1);
+			hours2 = parseInt(secondscamp / 3600);
+			secondscamp = secondscamp % 3600;
+			minutes2 = parseInt(secondscamp / 60);
+			secondscamp = secondscamp % 60;
+			el.innerHTML = minutes2 + ":" + secondscamp;
+		}
+		tempo = tempo - 1;
+		setTimeout("aguarda()", 1000);
+		}if (tempo == 0) {
+            var playplayplay = document.getElementById('audio');
+            function play(){
+                playplayplay.play();
+            }
+            }else{
+            } 
+}
+aguarda();
