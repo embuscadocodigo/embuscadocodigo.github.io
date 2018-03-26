2aee9a8fd16d358cee3448fcfa38f11a573f21b8
embuscadocodigo <31712288+embuscadocodigo@users.noreply.github.com>
2018-03-26 17:41:17 -0300

    Add files via upload

---
+++ js/premier.js
@@ -0,0 +1,252 @@
+function c01() {
+    var display = document.querySelector('.f01').style.display;
+    if(display == "block")
+        document.querySelector('.f01').style.display = 'none';
+    else
+        document.querySelector('.f01').style.display = 'block';
+}
+function c02() {
+    var display = document.querySelector('.f02').style.display;
+    if(display == "block")
+        document.querySelector('.f02').style.display = 'none';
+    else
+        document.querySelector('.f02').style.display = 'block';
+}
+function c03() {
+    var display = document.querySelector('.f03').style.display;
+    if(display == "block")
+        document.querySelector('.f03').style.display = 'none';
+    else
+        document.querySelector('.f03').style.display = 'block';
+}
+function c04() {
+    var display = document.querySelector('.f04').style.display;
+    if(display == "block")
+        document.querySelector('.f04').style.display = 'none';
+    else
+        document.querySelector('.f04').style.display = 'block';
+}
+function c05() {
+    var display = document.querySelector('.f05').style.display;
+    if(display == "block")
+        document.querySelector('.f05').style.display = 'none';
+    else
+        document.querySelector('.f05').style.display = 'block';
+}
+function c06() {
+    var display = document.querySelector('.f06').style.display;
+    if(display == "block")
+        document.querySelector('.f06').style.display = 'none';
+    else
+        document.querySelector('.f06').style.display = 'block';
+}
+function c07() {
+    var display = document.querySelector('.f07').style.display;
+    if(display == "block")
+        document.querySelector('.f07').style.display = 'none';
+    else
+        document.querySelector('.f07').style.display = 'block';
+}
+function c08() {
+    var display = document.querySelector('.f08').style.display;
+    if(display == "block")
+        document.querySelector('.f08').style.display = 'none';
+    else
+        document.querySelector('.f08').style.display = 'block';
+}
+function c09() {
+    var display = document.querySelector('.f09').style.display;
+    if(display == "block")
+        document.querySelector('.f09').style.display = 'none';
+    else
+        document.querySelector('.f09').style.display = 'block';
+}
+function c10() {
+    var display = document.querySelector('.f10').style.display;
+    if(display == "block")
+        document.querySelector('.f10').style.display = 'none';
+    else
+        document.querySelector('.f10').style.display = 'block';
+}
+function c11() {
+    var display = document.querySelector('.f11').style.display;
+    if(display == "block")
+        document.querySelector('.f11').style.display = 'none';
+    else
+        document.querySelector('.f11').style.display = 'block';
+}
+function c12() {
+    var display = document.querySelector('.f12').style.display;
+    if(display == "block")
+        document.querySelector('.f12').style.display = 'none';
+    else
+        document.querySelector('.f12').style.display = 'block';
+}
+function c13() {
+    var display = document.querySelector('.f13').style.display;
+    if(display == "block")
+        document.querySelector('.f13').style.display = 'none';
+    else
+        document.querySelector('.f13').style.display = 'block';
+}
+function c14() {
+    var display = document.querySelector('.f14').style.display;
+    if(display == "block")
+        document.querySelector('.f14').style.display = 'none';
+    else
+        document.querySelector('.f14').style.display = 'block';
+}
+function c15() {
+    var display = document.querySelector('.f15').style.display;
+    if(display == "block")
+        document.querySelector('.f15').style.display = 'none';
+    else
+        document.querySelector('.f15').style.display = 'block';
+}
+function c16() {
+    var display = document.querySelector('.f16').style.display;
+    if(display == "block")
+        document.querySelector('.f16').style.display = 'none';
+    else
+        document.querySelector('.f16').style.display = 'block';
+}
+function c17() {
+    var display = document.querySelector('.f17').style.display;
+    if(display == "block")
+        document.querySelector('.f17').style.display = 'none';
+    else
+        document.querySelector('.f17').style.display = 'block';
+}
+function c18() {
+    var display = document.querySelector('.f18').style.display;
+    if(display == "block")
+        document.querySelector('.f18').style.display = 'none';
+    else
+        document.querySelector('.f18').style.display = 'block';
+}
+function c19() {
+    var display = document.querySelector('.f19').style.display;
+    if(display == "block")
+        document.querySelector('.f19').style.display = 'none';
+    else
+        document.querySelector('.f19').style.display = 'block';
+}
+function c20() {
+    var display = document.querySelector('.f20').style.display;
+    if(display == "block")
+        document.querySelector('.f20').style.display = 'none';
+    else
+        document.querySelector('.f20').style.display = 'block';
+}
+function c21() {
+    var display = document.querySelector('.f21').style.display;
+    if(display == "block")
+        document.querySelector('.f21').style.display = 'none';
+    else
+        document.querySelector('.f21').style.display = 'block';
+}
+function c22() {
+    var display = document.querySelector('.f22').style.display;
+    if(display == "block")
+        document.querySelector('.f22').style.display = 'none';
+    else
+        document.querySelector('.f22').style.display = 'block';
+}
+function c23() {
+    var display = document.querySelector('.f23').style.display;
+    if(display == "block")
+        document.querySelector('.f23').style.display = 'none';
+    else
+        document.querySelector('.f23').style.display = 'block';
+}
+function c24() {
+    var display = document.querySelector('.f24').style.display;
+    if(display == "block")
+        document.querySelector('.f24').style.display = 'none';
+    else
+        document.querySelector('.f24').style.display = 'block';
+}
+function c25() {
+    var display = document.querySelector('.f25').style.display;
+    if(display == "block")
+        document.querySelector('.f25').style.display = 'none';
+    else
+        document.querySelector('.f25').style.display = 'block';
+}
+function c26() {
+    var display = document.querySelector('.f26').style.display;
+    if(display == "block")
+        document.querySelector('.f26').style.display = 'none';
+    else
+        document.querySelector('.f26').style.display = 'block';
+}
+function c27() {
+    var display = document.querySelector('.f27').style.display;
+    if(display == "block")
+        document.querySelector('.f27').style.display = 'none';
+    else
+        document.querySelector('.f27').style.display = 'block';
+}
+function c28() {
+    var display = document.querySelector('.f28').style.display;
+    if(display == "block")
+        document.querySelector('.f28').style.display = 'none';
+    else
+        document.querySelector('.f28').style.display = 'block';
+}
+function c29() {
+    var display = document.querySelector('.f29').style.display;
+    if(display == "block")
+        document.querySelector('.f29').style.display = 'none';
+    else
+        document.querySelector('.f29').style.display = 'block';
+}
+function c30() {
+    var display = document.querySelector('.f30').style.display;
+    if(display == "block")
+        document.querySelector('.f30').style.display = 'none';
+    else
+        document.querySelector('.f30').style.display = 'block';
+}
+function c31() {
+    var display = document.querySelector('.f31').style.display;
+    if(display == "block")
+        document.querySelector('.f31').style.display = 'none';
+    else
+        document.querySelector('.f31').style.display = 'block';
+}
+function c32() {
+    var display = document.querySelector('.f32').style.display;
+    if(display == "block")
+        document.querySelector('.f32').style.display = 'none';
+    else
+        document.querySelector('.f32').style.display = 'block';
+}
+function c33() {
+    var display = document.querySelector('.f33').style.display;
+    if(display == "block")
+        document.querySelector('.f33').style.display = 'none';
+    else
+        document.querySelector('.f33').style.display = 'block';
+}
+function c34() {
+    var display = document.querySelector('.f34').style.display;
+    if(display == "block")
+        document.querySelector('.f34').style.display = 'none';
+    else
+        document.querySelector('.f34').style.display = 'block';
+}
+function c35() {
+    var display = document.querySelector('.f35').style.display;
+    if(display == "block")
+        document.querySelector('.f35').style.display = 'none';
+    else
+        document.querySelector('.f35').style.display = 'block';
+}
+function c36() {
+    var display = document.querySelector('.f36').style.display;
+    if(display == "block")
+        document.querySelector('.f36').style.display = 'none';
+    else
+        document.querySelector('.f36').style.display = 'block';
+}
